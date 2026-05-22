a378685dc5ff36b7f42cc9886c93a66247424937
Cleidson Danilo <cleidsondanilo18@gmail.com>
2026-05-21 23:03:23 -0300

    organização da pagina detalhes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,8 +11,18 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./home/home.page').then((m) => m.HomePage),
   },
+
+  // ✅ Integrante 7 — Rota dinâmica com parâmetro :id
+  // Ao navegar para /detalhes/123, o Angular captura "123" como o parâmetro "id"
+  {
+    path: 'detalhes/:id',
+    loadComponent: () =>
+      import('./detalhes/detalhes.page').then((m) => m.DetalhesPage),
+  },
+
   {
     path: 'buscar',
-    loadComponent: () => import('./buscar/buscar.page').then( m => m.BuscarPage)
+    loadComponent: () => import
+    ('./buscar/buscar.page').then( m => m.BuscarPage)
   },
 ];
