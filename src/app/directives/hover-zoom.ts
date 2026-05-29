393cccbe659b1ef3b1fcc02d069bd403f652a816
Daniel-olv <danieloliveiramq@gmail.com>
2026-05-28 23:46:26 -0300

    ...

--- src/app/directives/hover-zoom.ts
+++ src/app/directives/hover-zoom.ts
@@ -7,17 +7,15 @@ import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 export class HoverZoom {
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
-    // Aplica a transição para o zoom ser suave
+    
     this.renderer.setStyle(this.el.nativeElement, 'transition', 'transform 0.3s ease-in-out');
   }
 
-  // Aumenta o card quando o mouse entra
   @HostListener('mouseenter') onMouseEnter() {
     this.renderer.setStyle(this.el.nativeElement, 'transform', 'scale(1.05)');
-    this.renderer.setStyle(this.el.nativeElement, 'cursor', 'pointer'); // Muda a setinha do mouse para a "mãozinha"
+    this.renderer.setStyle(this.el.nativeElement, 'cursor', 'pointer');
   }
 
-  // Volta ao tamanho normal quando o mouse sai
   @HostListener('mouseleave') onMouseLeave() {
     this.renderer.setStyle(this.el.nativeElement, 'transform', 'scale(1)');
   }
